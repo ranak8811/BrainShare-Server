a48c98cf991f7a36c1be1953ecbb63378cb54293
Anwar_Hossain <ranakrphone@gmail.com>
2025-01-18 04:10:23 +0600

    add post url done

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const morgan = require("morgan");
 const port = process.env.PORT || 4000;
 const app = express();
 
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  optionalSuccessStatus: 200,
+};
+
+// app.use(cors(corsOptions));
 app.use(cors());
 
 app.use(express.json());
@@ -26,6 +33,23 @@ const client = new MongoClient(uri, {
   },
 });
 
+// const verifyToken = (req, res, next) => {
+//   // console.log("its me to verify");
+//   const token = req.cookies?.token;
+//   // console.log(token);
+//   if (!token) {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "Unauthorized access" });
+//     }
+//     req.user = decoded;
+//   });
+//   next();
+// };
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -38,6 +62,47 @@ async function run() {
 
     const db = client.db("BrainShareDB");
     const usersCollection = db.collection("users");
+    const postsCollection = db.collection("posts");
+
+    // // generating jwt
+    // app.post("/jwt", async (req, res) => {
+    //   const email = req.body;
+    //   // token creation
+    //   const token = jwt.sign(email, process.env.SECRET_KEY, {
+    //     expiresIn: "300d",
+    //   });
+
+    //   // console.log(token);
+    //   res
+    //     .cookie("token", token, {
+    //       httpOnly: true,
+    //       secure: process.env.NODE_ENV === "production",
+    //       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    //     })
+    //     .send({ success: true });
+    // });
+
+    // // logout or clear cookie from browser
+    // app.get("/logout", async (req, res) => {
+    //   res
+    //     .clearCookie("token", {
+    //       maxAge: 0,
+    //       secure: process.env.NODE_ENV === "production",
+    //       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    //     })
+    //     .send({ success: true });
+    // });
+
+    // add a post to db
+    app.post("/add-post", async (req, res) => {
+      const postData = req.body;
+      const result = await postsCollection.insertOne({
+        ...postData,
+        upVote: 0,
+        downVote: 0,
+      });
+      res.send(result);
+    });
 
     // save or update users in db
     app.post("/users/:email", async (req, res) => {
