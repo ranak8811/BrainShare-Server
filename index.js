3f6fe1abe7c83840fe6dfd69e247fbd724f36664
Anwar_Hossain <ranakrphone@gmail.com>
2025-01-18 12:46:35 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-// const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const morgan = require("morgan");
 
 const port = process.env.PORT || 4000;
@@ -15,11 +15,11 @@ const corsOptions = {
   optionalSuccessStatus: 200,
 };
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+// app.use(cors());
 
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(morgan("dev"));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j5yqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -33,22 +33,22 @@ const client = new MongoClient(uri, {
   },
 });
 
-// const verifyToken = (req, res, next) => {
-//   // console.log("its me to verify");
-//   const token = req.cookies?.token;
-//   // console.log(token);
-//   if (!token) {
-//     return res.status(401).send({ message: "Unauthorized access" });
-//   }
+const verifyToken = (req, res, next) => {
+  // console.log("its me to verify");
+  const token = req.cookies?.token;
+  // console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
 
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.status(401).send({ message: "Unauthorized access" });
-//     }
-//     req.user = decoded;
-//   });
-//   next();
-// };
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized access" });
+    }
+    req.user = decoded;
+  });
+  next();
+};
 
 async function run() {
   try {
@@ -64,37 +64,37 @@ async function run() {
     const usersCollection = db.collection("users");
     const postsCollection = db.collection("posts");
 
-    // // generating jwt
-    // app.post("/jwt", async (req, res) => {
-    //   const email = req.body;
-    //   // token creation
-    //   const token = jwt.sign(email, process.env.SECRET_KEY, {
-    //     expiresIn: "300d",
-    //   });
+    // generating jwt
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      // token creation
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "300d",
+      });
 
-    //   // console.log(token);
-    //   res
-    //     .cookie("token", token, {
-    //       httpOnly: true,
-    //       secure: process.env.NODE_ENV === "production",
-    //       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    //     })
-    //     .send({ success: true });
-    // });
+      // console.log(token);
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
-    // // logout or clear cookie from browser
-    // app.get("/logout", async (req, res) => {
-    //   res
-    //     .clearCookie("token", {
-    //       maxAge: 0,
-    //       secure: process.env.NODE_ENV === "production",
-    //       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    //     })
-    //     .send({ success: true });
-    // });
+    // logout or clear cookie from browser
+    app.get("/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
 
     // add a post to db
-    app.post("/add-post", async (req, res) => {
+    app.post("/add-post", verifyToken, async (req, res) => {
       const postData = req.body;
       const result = await postsCollection.insertOne({
         ...postData,
